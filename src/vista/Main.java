b0e420abd19a881e82a942eb82c7bd84be937be8
andresrodriguez2 <167079094+andresrodriguez2@users.noreply.github.com>
2024-12-15 19:53:23 +0100

    Se agrega el proyecto

---
+++ src/vista/Main.java
@@ -0,0 +1,28 @@
+package proyectoEntornos.vista;
+
+import proyectoEntornos.asistenciaAlumno.asisAlumno;
+
+public class Main {
+    public static void main(String[] args) {
+
+asisAlumno estudio=new asisAlumno();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
